5442c88ce72b61a5cf2e1d2911287d9345fbad23
Andre Didier <andredidier@users.noreply.github.com>
2018-08-25 05:00:57 -0300

    Added static project. Removed Invoice class.

--- scan-invoice-api/src/main/java/com/lealdidier/invoice/scan/Invoice.java
+++
@@ -1,9 +0,0 @@
-package com.lealdidier.invoice.scan;
-
-import spark.Response;
-
-import java.io.IOException;
-
-public interface Invoice {
-    void respondWithJsonBody(Response response) throws IOException;
-}
